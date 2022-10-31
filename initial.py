e5ecfeaf8fa0ed1e7fecd2cd3062e627d0de28bb
Harsh Verma <harshkardam246@gmail.com>
2022-10-31 09:10:06 +0530

    Create initial.py

---
+++ initial.py
@@ -0,0 +1,18 @@
+import mysql.connector as mc
+mydb = mc.connect(user="root", host="localhost", passwd="root")
+mycursor = mydb.cursor()
+
+#Create databases;
+print("Creating Databases :- ")
+mycursor.execute("create databases bookshop;")
+mycursor.execute("use bookshop")
+
+#Create all tables in databases;
+print("Creating Table :- ")
+mycursor.execute("create table book(book_id int primary key, book_name varchar(100), book_author varchar(30) not null, genre varchar(30), book_price int not null);")
+mycursor.execute("create table issue(book_id int, client_name varchar(30), foreign key (book_id) references book(book_id));")
+mycursor.execute("create table buyrequests(book_id int, client_name varchar(30), foreign key (book_id) references book(book_id));")
+
+#Notify user.
+print("All Done.")
+print("You can insert book data using 'admin.py' file.")
