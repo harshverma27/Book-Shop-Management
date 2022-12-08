1b21cafece2c17bae498f255e8da8f9e6727fedd
Harsh Verma <harshkardam246@gmail.com>
2022-12-08 10:07:48 +0530

    Update initial.py

--- initial.py
+++ initial.py
@@ -4,7 +4,7 @@ mycursor = mydb.cursor()
 
 # This fucntion creates a database in mySQL
 def mysqlSetup():
-    mycursor.execute("create if not exits database bookshop;")
+    mycursor.execute("create database if not exists bookshop;")
     mycursor.execute("use bookshop")
     mycursor.execute("create table if not exists book(book_id int primary key, book_name varchar(100) not null, book_author varchar(50), genre varchar(30), book_price int );")
     mycursor.execute("create table if not exists issue(book_id int, client_name varchar(30), foreign key (book_id) references book(book_id));")
